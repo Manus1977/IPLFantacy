301c78ba1cea63b7a19fac9551f61f6093761c51
unknown <srdmb10@gmail.com>
2022-04-13 14:26:59 +0530

    provide comment

---
+++ src/main/java/ipl/IplFantacyApplication.java
@@ -0,0 +1,13 @@
+package ipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplFantacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplFantacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ipl/IplFantacyApplicationTests.java
@@ -0,0 +1,13 @@
+package ipl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplFantacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
